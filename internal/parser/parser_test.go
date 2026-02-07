cfa4c5e5f62cc24a0945abf7f0ae72a4f62588cb
Alex <ak.frick@promatis.ru>
2026-02-07 14:32:14 +0500

    broke the test

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -10,8 +10,14 @@ func TestExample(t *testing.T) {
 }
 
 func TestExample2(t *testing.T) {
-	sum := 2 + 2
-	if sum != 4 {
+	sum := 2 + 3
+	if sum != 5 {
+		t.Errorf("Expected 4, got %d", sum)
+	}
+}
+func TestExample3(t *testing.T) {
+	sum := 3 + 3
+	if sum != 6 {
 		t.Errorf("Expected 4, got %d", sum)
 	}
 }
